06fbf6eea9af6295790e83863ae33a98d475906d
Andrew71 <77640396+Andrew-71@users.noreply.github.com>
2021-11-20 00:01:57 +0300

    Spent 3 hours on bug fixes. But hey, at least the code works now!

--- crappit.py
+++ crappit.py
@@ -1,6 +1,6 @@
 import sys  # Used for... idk?
 import re  # Used for formatting.
-from PyQt5.QtWidgets import QApplication, QMainWindow, QWidget, QDialog  # All types of windows we use.
+from PyQt5.QtWidgets import QApplication, QMainWindow, QWidget, QDialog, QMessageBox  # All types of windows we use.
 import praw  # For communications with Reddit.
 import prawcore  # For handling Reddit exceptions.
 import urllib.request  # Handles download of images for image posts.
@@ -8,7 +8,7 @@ import pyperclip  # For copying link to post.
 import sqlite3  # For databases. I hate those things.
 import time  # For displaying time since submission was posted
 
-import app_ui
+import app_ui  # Import UI
 
 
 # ==================================================================================================
@@ -17,25 +17,25 @@ import app_ui
 
 def format_text(text):
     pattern_link = re.compile(r'\[.*?\]\[.*?\]')  # Pattern to find links  (weird format)
-    pattern_link_normal = re.compile(r'\[.*?\]\(.*?\)')  # Pattern to find links (classic format)
+    pattern_link_normal = re.compile(r'\[.*?\]\(.*?\)')  # Pattern to find links (normal format)
     pattern_bold = re.compile(r'\*{2}(.*?)\*{2}')  # Pattern to find bold text
     pattern_italics = re.compile(r'\*(.*?)\*')  # Pattern to find italics
 
     formatted_text = ''  # Output
 
     if pattern_link.search(text):  # If there are weird links in text that means lowest paragraph has definitions
-        link_meaning = {}
+        link_meaning = {}  # Dictionary with all links and their meaning
         for i in text.split('\n\n')[-1].split('\n'):
-            link = i.strip().split(']: ')
-            if len(link) == 0 or len(link[0]) == 0 or len(link[1]) == 0:
+            link = i.strip().split(']: ')  # Split line into name of link and it's website
+            if len(link) == 0 or len(link[0]) == 0 or len(link[1]) == 0:  # If one of them is empty skip it.
                 continue
             link_meaning[link[0][1:]] = link[1]
         text = text.rsplit('\n\n', maxsplit=1)[0]  # Remove links definitions from actual text
 
-    # Replace all WEIRD in-text links with HTML hyperlinks  [text][link_key]
-    for i in pattern_link.findall(text):
-        link = i[1:-1].split('][')
-        text = text.replace(i, f'<a href="{link_meaning[link[1]]}">{link[0]}</a>')
+        # Replace all WEIRD in-text links with HTML hyperlinks  [text][link_key]
+        for i in pattern_link.findall(text):
+            link = i[1:-1].split('][')
+            text = text.replace(i, f'<a href="{link_meaning[link[1]]}">{link[0]}</a>')
 
     # Replace all NORMAL in-text links with HTML hyperlinks  [text](link)
     for i in pattern_link_normal.findall(text):
@@ -55,10 +55,11 @@ def format_text(text):
 
     # Make big text big  (#text)
     for line in text.split('\n'):
-        if len(line) != 0 and line[0] == '#':
-            formatted_line = f'<span style="font-size: 24px">{line}</span>'
+        if len(line) != 0 and line[0] == '#':  # If we find '#' at start of line that means that this line is big
+            formatted_line = f'<span style="font-size: 24px">{line}</span>'  # Make it   B I G
         else:
-            formatted_line = line
+            formatted_line = line  # Else just use standard sized line
+
         formatted_line = formatted_line.replace('#', '')  # Remove '#' symbol from the line
         formatted_text += f'<p>{formatted_line}</p>'  # Put the line in paragraph to make it appear as separate line
 
@@ -73,6 +74,8 @@ def user_with_link(post):
     try:
         user = post.author.name  # Name of author of post
         user = f' <a href="https://www.reddit.com/user/{user}">u/{user}</a>'  # Clicking on username opens profile
+
+    # TODO: Reconsider putting specific exception.
     except:  # User is deleted. I tried putting specific exception here (prawcore.NotFound) but it doesn't seem to work
         user = 'u/[DELETED]'
     return user
@@ -103,18 +106,18 @@ def check_for_credentials(i):
 
 # Input is string, outputs it with '\n' added in some places to fit it in window
 def text_on_multiple_lines(text, line_len):
-    new_text = ' '  # Output title
+    new_text = ''  # Output text
     length = 0  # Length of current line
 
     for i in text.split():
         if length + len(i) > line_len:  # If new word wouldn't fit on the line
             length = len(i) + 1  # Change length to new line
-            new_text += '\n '  # Switch to next line
+            new_text += '\n'  # Switch to next line
         else:
             length += len(i) + 1  # Else increase length
         new_text += i + ' '  # Add word to current line
 
-    return new_text  # Return new title, now complete with \n (tm)
+    return new_text  # Return new text, now complete with \n (tm)
 
 
 # Input is reddit post, returns "d h m" type string with time since post was submitted
@@ -158,7 +161,7 @@ class MainWindow(QMainWindow, app_ui.MainUi):
     def __init__(self, login_info):
         super().__init__()
         self.setupUi(self)
-        self.setFixedSize(530, 750)  # Set fixed size. A sacrifice in order for image scaling to work.
+        self.setFixedSize(550, 760)  # Set fixed size. A sacrifice in order for image scaling to work.
 
         # Honestly, I am making reddit global here because most of code is inherited from when it was global
         # And I am not wasting 10 minutes adding 'self.' to all mentions of this variable.
@@ -202,6 +205,7 @@ class MainWindow(QMainWindow, app_ui.MainUi):
 
         self.body_text.setOpenExternalLinks(True)  # Make it so the hyperlinks in text work.
         self.username_text.setOpenExternalLinks(True)  # Enable link to OP's profile
+        self.subreddit_text.setOpenExternalLinks(True)  # Enable links to subreddits
 
         # Connect buttons for controlling what posts you see
         self.sorting_btn.clicked.connect(self.sort_selection)
@@ -219,32 +223,41 @@ class MainWindow(QMainWindow, app_ui.MainUi):
         if len(user_subreddits) == 0:
             user_subreddits = ['all']
         else:
-            user_subreddits = list(map(lambda x: x[0], user_subreddits))
+            user_subreddits = list(map(lambda x: str(x[0]), user_subreddits))
 
         self.posts = []  # Empty the list with post ids  (or create it if we refresh for the first time)
 
         # Yes. This is, in fact, most efficient method.
         # Because Reddit doesn't let you parse through posts with sorting as a parameter
         # So we need to call different methods
+
+        # Also you know what, screw this
+        # Any posts that don't have text aren't recognised
+        # Wanna know why? Because there is almost no way to differentiate them from videos
         if self.sorting_method[0] == 'hot':
             for i in reddit.subreddit('+'.join(user_subreddits)).hot(limit=50):
-                if len(i.selftext) != 0 or 'i.redd.it' in i.url:  # Only choose text posts and officially hosted images
+                # Only choose text posts and officially hosted images
+                if len(i.selftext) > 0 or 'i.redd.it' in i.url:
                     self.posts.append(i.id)
         elif self.sorting_method[0] == 'new':
             for i in reddit.subreddit('+'.join(user_subreddits)).new(limit=50):
-                if len(i.selftext) != 0 or 'i.redd.it' in i.url:  # Only choose text posts and officially hosted images
+                # Only choose text posts and officially hosted images
+                if len(i.selftext) > 0 or 'i.redd.it' in i.url:
                     self.posts.append(i.id)
         elif self.sorting_method[0] == 'rising':
             for i in reddit.subreddit('+'.join(user_subreddits)).rising(limit=50):
-                if len(i.selftext) != 0 or 'i.redd.it' in i.url:  # Only choose text posts and officially hosted images
+                # Only choose text posts and officially hosted images
+                if len(i.selftext) > 0 or 'i.redd.it' in i.url:
                     self.posts.append(i.id)
         elif self.sorting_method[0] == 'top':
             for i in reddit.subreddit('+'.join(user_subreddits)).top(self.sorting_method[1], limit=50):
-                if len(i.selftext) != 0 or 'i.redd.it' in i.url:  # Only choose text posts and officially hosted images
+                # Only choose text posts and officially hosted images
+                if len(i.selftext) > 0 or 'i.redd.it' in i.url:
                     self.posts.append(i.id)
         elif self.sorting_method[0] == 'controversial':
             for i in reddit.subreddit('+'.join(user_subreddits)).controversial(self.sorting_method[1], limit=50):
-                if len(i.selftext) != 0 or 'i.redd.it' in i.url:  # Only choose text posts and officially hosted images
+                # Only choose text posts and officially hosted images
+                if len(i.selftext) > 0 or 'i.redd.it' in i.url:
                     self.posts.append(i.id)
 
         if len(self.posts) > 0:  # If there is at least 1 post
@@ -258,9 +271,13 @@ class MainWindow(QMainWindow, app_ui.MainUi):
             self.toggle_interaction_buttons(True)
 
         else:
+            # Just show the message that there weren't any posts
+
             self.title_text.setText('No posts found')  # Tell user there aren't any posts
-            self.body_text.setText('Wow, such empty!')
-            self.username_text.setTexr('')
+            self.body_text.setText('Wow, such empty!')  # Reddit website reference
+            self.username_text.setText('')
+            self.subreddit_text.setText('')
+            self.time_text.setText('')
 
             # Deactivate buttons used to interact with posts... because there aren't any
             self.toggle_interaction_buttons(False)
@@ -270,7 +287,6 @@ class MainWindow(QMainWindow, app_ui.MainUi):
         self.downvote_btn.setEnabled(value)
         self.comment_btn.setEnabled(value)
         self.share_btn.setEnabled(value)
-        self.report_btn.setEnabled(value)
 
     def vote_post(self, up_or_down):
         try:
@@ -314,7 +330,9 @@ class MainWindow(QMainWindow, app_ui.MainUi):
     def show_post(self, post_id):
         post = reddit.submission(id=post_id)  # Get id of submission we should show
 
-        if len(post.selftext) == 0:
+        # If it's an image show it else show the body text
+        if 'i.redd.it' in post.url:
+            # This code screws over tall and wide images, but it's a sacrifice I am willing to take
             urllib.request.urlretrieve(post.url, "crappit-image-hash.jpg")
             self.body_text.setText(f'<img src="crappit-image-hash.jpg" width = 520 height = 470>')
         else:
@@ -336,6 +354,7 @@ class MainWindow(QMainWindow, app_ui.MainUi):
             score = 'vote'  # If we got an error that means score is hidden so we put 'vote' like on the Reddit website
         self.score_label.setText(str(score))
 
+        # Display state of vote button on this post
         vote = check_vote(post)
         if vote is None:
             self.colour_button()
@@ -344,7 +363,9 @@ class MainWindow(QMainWindow, app_ui.MainUi):
         else:
             self.colour_button('blue')
 
-        self.subreddit_and_time_text.setText(f' r/{str(post.subreddit)} \n Posted {time_since_post(post)} ago')
+        self.subreddit_text.setText(f'<a href="https://www.reddit.com/r/r/{str(post.subreddit)}">'
+                                    f'r/{str(post.subreddit)}</a>')
+        self.time_text.setText(f'Posted {time_since_post(post)} ago')
 
     def sort_selection(self):
         self.sort_window = SortingSelectWindow(self.user_id)
@@ -540,7 +561,8 @@ class CommentCreationWindow(QWidget, app_ui.SubmitCommentUi):
             post.reply(text)
             self.hide()
         except:
-            app_submit_comment_error = MessageWindow('Unable to submit comment', 'The post might be locked or deleted'                                                '\nOr you might even be banned')
+            app_submit_comment_error = MessageWindow('Unable to submit comment', 'The post might be locked or deleted'
+                                                                                 '\nOr you might even be banned')
             app_submit_comment_error.show()
             self.hide()
 
@@ -575,6 +597,7 @@ class SubredditSelectWindow(QWidget, app_ui.SubredditSelectUi):
         self.done_btn.clicked.connect(self.done)
 
         self.import_btn.clicked.connect(self.import_subs)
+        self.export_btn.clicked.connect(self.export_subs)
 
     def add_sub(self):
         sub_name = str(self.subreddit_name.text())
@@ -583,23 +606,26 @@ class SubredditSelectWindow(QWidget, app_ui.SubredditSelectUi):
                 if sub_name[:2] == 'r/':
                     sub_name = sub_name[2:]
 
+                sub_name = sub_name.lower().capitalize()  # Format subreddit name to not have duplicates
+
                 reddit.subreddits.search_by_name(sub_name, exact=True)  # Try accessing subreddit
 
                 if len(cur.execute(f"""SELECT * FROM Subreddits WHERE id = ? and Subreddit_name = ?""",
                                    (self.id_num, sub_name)).fetchall()) < 1:
                     cur.execute("""INSERT INTO Subreddits VALUES (?, ?)""",
-                                (self.id_num, sub_name.lower().capitalize()))
+                                (self.id_num, str(sub_name)))
                     con.commit()
                     self.refresh()
 
             else:
                 raise Exception
-        except:
+        except Exception as e:
+            print(e)
             app_subreddit_add_error = MessageWindow('Unable to add subreddit', 'Please check your spelling')
             app_subreddit_add_error.show()
 
     def refresh(self):
-        self.subreddits = sorted(list(map(lambda x: x[0], cur.execute(f"""SELECT Subreddit_name FROM Subreddits 
+        self.subreddits = sorted(list(map(lambda x: str(x[0]), cur.execute(f"""SELECT Subreddit_name FROM Subreddits 
                 WHERE id = {self.id_num}""").fetchall())))
         self.sub_list.clear()
         self.sub_list.addItems(self.subreddits)
@@ -608,18 +634,43 @@ class SubredditSelectWindow(QWidget, app_ui.SubredditSelectUi):
         sub_name = str(self.subreddit_name.text())
         if len(sub_name) > 3 and sub_name[:2] == 'r/':
             sub_name = sub_name[2:]
+        sub_name = sub_name.lower().capitalize()  # Format subreddit name to not have duplicates
         cur.execute(f"""DELETE FROM Subreddits WHERE id = ? and Subreddit_name = ?""", (self.id_num, str(sub_name)))
         con.commit()
         self.refresh()
 
     def import_subs(self):
-        # Basically a refresh except we use user's subbed subreddits
-        self.subreddits = sorted(list(map(lambda x: x.display_name, list(reddit.user.subreddits(limit=None)))))
-        for i in self.subreddits:
-            cur.execute("""INSERT INTO Subreddits VALUES (?, ?)""", (self.id_num, str(i)))
-            con.commit()
-        self.sub_list.clear()
-        self.sub_list.addItems(self.subreddits)
+        # God bless stackoverflow and their QMessageBox tutorials
+        confirm_window = QMessageBox
+        ret = confirm_window.question(self, '', "Are you sure you want to import from account?",
+                                      confirm_window.Yes | confirm_window.No)
+
+        if ret == confirm_window.Yes:
+            # Basically a refresh except we use user's subbed subreddits
+            self.subreddits = sorted(list(map(lambda x: x.display_name.lower().capitalize(),
+                                              list(reddit.user.subreddits(limit=None)))))
+            for i in self.subreddits:
+                cur.execute("""INSERT INTO Subreddits VALUES (?, ?)""", (self.id_num, str(i)))
+                con.commit()
+            self.sub_list.clear()
+            self.sub_list.addItems(self.subreddits)
+
+    def export_subs(self):
+        # Once again, may the sun never set on the StackOverflow empire
+        confirm_window = QMessageBox
+        ret = confirm_window.question(self, '', "Are you sure you want to export to account?",
+                                      confirm_window.Yes | confirm_window.No)
+
+        if ret == confirm_window.Yes:
+            old_subreddits = sorted(list(map(lambda x: x.display_name, list(reddit.user.subreddits(limit=None)))))
+            new_subreddits = sorted(list(map(lambda x: str(x[0]), cur.execute(f"""SELECT Subreddit_name FROM Subreddits 
+                            WHERE id = {self.id_num}""").fetchall())))
+            for i in old_subreddits:
+                reddit.subreddit(i).unsubscribe()
+
+            for i in new_subreddits:
+                reddit.subreddit(i).subscribe()
+
 
     def done(self):
         login_window.app_main_window.refresh_posts()
@@ -724,6 +775,7 @@ class LoginWindowEdit(QWidget, app_ui.LoginEditUi):
     def __init__(self, id_num):
         super().__init__()
         self.setupUi(self)
+        self.setFixedSize(480, 350)  # Fix the awful design of this window that's been annoying me since day 1
 
         self.save_btn.clicked.connect(self.save)  # Connect Save Button
         self.delete_btn.clicked.connect(self.delete)  # Connect Delete Button
